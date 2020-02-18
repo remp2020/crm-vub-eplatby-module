3b223c57877a3ac17479eb7377268eb1c9a446e0
Peter Dulacka <dulacka@gmail.com>
2020-02-18 12:39:05 +0100

    Making configs movement as non-breaking change, cleaning up
    * All newly created mail configs now copy values from old mail
    config so they don't stop working after the release.
    * Configs were refactored to use common trait to maintain sorting
    category, and default values.
    * Some translations were not correct.
    
    remp/crm#1061

--- src/seeders/ConfigsSeeder.php
+++ src/seeders/ConfigsSeeder.php
@@ -6,11 +6,14 @@ use Crm\ApplicationModule\Builder\ConfigBuilder;
 use Crm\ApplicationModule\Config\ApplicationConfig;
 use Crm\ApplicationModule\Config\Repository\ConfigCategoriesRepository;
 use Crm\ApplicationModule\Config\Repository\ConfigsRepository;
+use Crm\ApplicationModule\Seeders\ConfigsTrait;
 use Crm\ApplicationModule\Seeders\ISeeder;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ConfigsSeeder implements ISeeder
 {
+    use ConfigsTrait;
+
     private $configCategoriesRepository;
 
     private $configsRepository;
@@ -32,133 +35,119 @@ class ConfigsSeeder implements ISeeder
         $category = $this->configCategoriesRepository->findBy('name', 'payments.config.category');
         $sorting = 1400;
 
-        $this->addPaymentConfig(
+        $this->addConfig(
             $output,
             $category,
             'vub_eplatby_sharedsecret',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.sharedsecret.name',
+            'vub_eplatby.config.sharedsecret.description',
             '1111111111111111111111111111111111111111111111111111111111111111',
-            $sorting++,
-            'vub_eplatby.config.sharedsecret.description'
+            $sorting++
         );
-        $this->addPaymentConfig(
+
+        $this->addConfig(
             $output,
             $category,
             'vub_eplatby_mid',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.mid.name',
+            'vub_eplatby.config.mid.description',
             '1111',
-            $sorting++,
-            'vub_eplatby.config.mid.description'
+            $sorting++
         );
-        $this->addPaymentConfig(
+
+        $this->addConfig(
             $output,
             $category,
             'vub_eplatby_mode',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.mode.name',
+            'vub_eplatby.config.mode.description',
             'test',
-            $sorting++,
-            'vub_eplatby.config.mode.description'
+            $sorting++
         );
 
-        $categoryName = 'payments.config.category_confirmation';
-        $category = $category = $this->configCategoriesRepository->loadByName($categoryName);
+        $category = $this->configCategoriesRepository->loadByName('payments.config.category_confirmation');
         if (!$category) {
-            $category = $category = $this->configCategoriesRepository->add($categoryName, 'fa fa-check-double', 1600);
+            $category = $this->configCategoriesRepository->add('payments.config.category_confirmation', 'fa fa-check-double', 1600);
             $output->writeln('  <comment>* config category <info>Potvrdzovacie e-maily</info> created</comment>');
         } else {
             $output->writeln('  * config category <info>Potvrdzovacie e-maily</info> exists');
         }
 
-        $this->addPaymentConfig(
+        $this->addConfig(
             $output,
             $category,
             'vub_confirmation_host',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.vub_confirmation_host.name',
+            'vub_eplatby.config.vub_confirmation_host.description',
             '',
-            200,
-            'vub_eplatby.config.vub_confirmation_host.description'
+            1001
         );
 
-        $this->addPaymentConfig(
+        $this->addConfig(
             $output,
             $category,
             'vub_confirmation_port',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.vub_confirmation_port.name',
+            'vub_eplatby.config.vub_confirmation_port.description',
             '',
-            201,
-            'vub_eplatby.config.vub_confirmation_port.description'
+            1002
         );
 
-        $this->addPaymentConfig(
+        $this->addConfig(
             $output,
             $category,
             'vub_confirmation_username',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.vub_confirmation_username.name',
+            'vub_eplatby.config.vub_confirmation_username.description',
             '',
-            202,
-            'vub_eplatby.config.vub_confirmation_username.description'
+            1003
         );
 
-        $this->addPaymentConfig(
+        $this->addConfig(
             $output,
             $category,
             'vub_confirmation_password',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.vub_confirmation_password.name',
+            'vub_eplatby.config.vub_confirmation_password.description',
             '',
-            203,
-            'vub_eplatby.config.vub_confirmation_password.description'
+            1004
         );
 
-        $this->addPaymentConfig(
+        $this->addConfig(
             $output,
             $category,
             'vub_confirmation_processed_folder',
+            ApplicationConfig::TYPE_STRING,
             'vub_eplatby.config.vub_confirmation_processed_folder.name',
+            'vub_eplatby.config.vub_confirmation_processed_folder.description',
             '',
-            204,
-            'vub_eplatby.config.vub_confirmation_processed_folder.description'
+            1005
         );
 
-        $this->addPaymentConfig(
+        $this->addConfig(
             $output,
             $category,
             'vub_zip_password',
-            'payments.config.vub_zip_password.name',
+            ApplicationConfig::TYPE_STRING,
+            'vub_eplatby.config.vub_zip_password.name',
+            'vub_eplatby.config.vub_zip_password.description',
             '',
-            205,
-            'payments.config.vub_zip_password.description'
+            1006
         );
-    }
 
-    private function addPaymentConfig(OutputInterface $output, $category, $name, $displayName, $value, $sorting, $description = null)
-    {
-        $config = $this->configsRepository->loadByName($name);
-        if (!$config) {
-            $this->configBuilder->createNew()
-                ->setName($name)
-                ->setDisplayName($displayName)
-                ->setDescription($description)
-                ->setValue($value)
-                ->setType(ApplicationConfig::TYPE_STRING)
-                ->setAutoload(true)
-                ->setConfigCategory($category)
-                ->setSorting($sorting)
-                ->save();
-            $output->writeln("  <comment>* config item <info>$name</info> created</comment>");
-        } else {
-            $output->writeln("  * config item <info>$name</info> exists");
-
-            if ($config->has_default_value && $config->value !== $value) {
-                $this->configsRepository->update($config, ['value' => $value, 'has_default_value' => true]);
-                $output->writeln("  <comment>* config item <info>$name</info> updated</comment>");
-            }
-
-            if ($config->category->name != $category->name) {
-                $this->configsRepository->update($config, [
-                    'config_category_id' => $category->id
-                ]);
-                $output->writeln("  <comment>* config item <info>$name</info> updated</comment>");
-            }
-        }
+        // moving configs to different category if they already existed
+        $config = $this->configsRepository->loadByName('vub_zip_password');
+        $this->configsRepository->update($config, [
+            'display_name' => 'vub_eplatby.config.vub_zip_password.name',
+            'description' => 'vub_eplatby.config.vub_zip_password.description',
+            'sorting' => 1006
+        ]);
     }
 }
