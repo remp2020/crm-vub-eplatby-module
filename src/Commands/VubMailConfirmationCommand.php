29ef6016331fa5d688f141a02b3669ac772f87c0
Adam Žoldák <zoldakadam@gmail.com>
2023-09-04 12:57:26 +0000

    Refactored payment confirmation commands
    remp/crm#2878
    - Removed redundant mail downloader classes (`CsobMailDownloader`,
    `SkCsobMailDownloader`, `TatraBankaMailDownloader`,
    `TatraBankaStatementMailDownloader`, `SlspMailDownloader`, `VubMailDownloader`)
    and moved logic directly to commands
    - Abstracted direct dependency on `Tomaj\ImapMailDownloader\Downloader`
    to `ImapMailDownloader`
    - Replaced direct dependency on `Tomaj\ImapMailDownloader\Downloader`
    with `MailDownloaderInterface` in confirmation commands
    - Added option to replace default mail downloader `ImapMailDownloader`
    (downloader must implement: `MailDownloaderInterface`)

--- src/Commands/VubMailConfirmationCommand.php
+++ src/Commands/VubMailConfirmationCommand.php
@@ -2,25 +2,33 @@
 
 namespace Crm\VubEplatbyModule\Commands;
 
+use Crm\ApplicationModule\Config\ApplicationConfig;
 use Crm\PaymentsModule\MailConfirmation\MailProcessor;
-use Crm\VubEplatbyModule\MailConfirmation\VubMailDownloader;
+use Crm\PaymentsModule\Models\MailDownloader\EmailInterface;
+use Crm\PaymentsModule\Models\MailDownloader\MailDownloaderInterface;
+use Crm\VubEplatbyModule\MailParser\VubMailParser;
+use Nette\Utils\FileSystem;
+use Nette\Utils\Random;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Tomaj\ImapMailDownloader\MailCriteria;
+use Tracy\Debugger;
+use Tracy\ILogger;
 
 class VubMailConfirmationCommand extends Command
 {
-    private $mailDownloader;
+    private OutputInterface $output;
 
-    private $mailProcessor;
+    private string $zipPassword;
 
     public function __construct(
-        VubMailDownloader $mailDownloader,
-        MailProcessor $mailProcessor
+        private string $tempDir,
+        private MailDownloaderInterface $mailDownloader,
+        private MailProcessor $mailProcessor,
+        private ApplicationConfig $applicationConfig,
     ) {
         parent::__construct();
-        $this->mailDownloader = $mailDownloader;
-        $this->mailProcessor = $mailProcessor;
     }
 
     protected function configure()
@@ -31,15 +39,81 @@ class VubMailConfirmationCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->mailDownloader->download(function ($mailContent) use ($output) {
-            return $this->markMailProcessed($mailContent, $output);
+        $this->output = $output;
+        $this->zipPassword = $this->applicationConfig->get('vub_zip_password');
+
+        $connectionOptions = [
+            'imapHost' => $this->applicationConfig->get('vub_confirmation_host'),
+            'imapPort' => $this->applicationConfig->get('vub_confirmation_port'),
+            'username' => $this->applicationConfig->get('vub_confirmation_username'),
+            'password' => $this->applicationConfig->get('vub_confirmation_password'),
+            'processedFolder' => $this->applicationConfig->get('vub_confirmation_processed_folder'),
+        ];
+
+        $criteria = new MailCriteria();
+        $criteria->setFrom('nonstopbanking@vub.sk');
+        $criteria->setUnseen(true);
+        $connectionOptions['criteria'] = $criteria;
+
+        $this->mailDownloader->download($connectionOptions, function (EmailInterface $email) {
+            $parser = new VubMailParser();
+
+            $body = $this->getAttachmentBody($email);
+            $mailContent = $parser->parse(quoted_printable_decode($body));
+            if ($mailContent !== null && $body) {
+                $this->mailProcessor->processMail($mailContent, $this->output);
+            }
         });
 
         return Command::SUCCESS;
     }
 
-    private function markMailProcessed($mailContent, $output)
+    private function validateAttachment(EmailInterface $email): bool
     {
-        return !$this->mailProcessor->processMail($mailContent, $output);
+        $attachments = $email->getAttachments();
+        if (empty($attachments)) {
+            $vs = '';
+            $result = [];
+            $body = $email->getBody();
+            $vsPattern = '/č.ob. (\d{10})/m';
+
+            $res = preg_match($vsPattern, $body, $result);
+            if ($res) {
+                $vs = $result[1];
+            }
+
+            Debugger::log(
+                'missing VUB mail attachment for payment with variable symbol: ' . $vs,
+                ILogger::WARNING
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private function getAttachmentBody(EmailInterface $email): bool|string
+    {
+        if (!$this->validateAttachment($email)) {
+            return false;
+        }
+
+        $filePath = $this->tempDir . DIRECTORY_SEPARATOR . 'payments-mail-parser/' . Random::generate() . '.zip';
+
+        FileSystem::write($filePath, array_values($email->getAttachments())[0]['attachment']);
+
+        $zip = new \ZipArchive();
+        $zip->open($filePath);
+        $zip->setPassword($this->zipPassword);
+
+        $contents = $zip->getFromIndex(0);
+        if (!$contents) {
+            throw new \Exception('error opening VUB confirmation e-mail attachment: probably bad password in CRM configuration)');
+        }
+
+        $contents = iconv("UTF-8", "ISO-8859-1//IGNORE", $contents);
+
+        FileSystem::delete($filePath);
+
+        return $contents;
     }
 }
