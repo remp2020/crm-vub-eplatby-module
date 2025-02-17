5f1b18667c8d296f64d35fa0724110e3aa8ea83d
Peter Dulacka <dulacka@gmail.com>
2025-02-17 06:16:05 +0000

    Update Nette extension config definition to use config definition schema
    remp/crm#3417

--- src/DI/VubEplatbyModuleExtension.php
+++ src/DI/VubEplatbyModuleExtension.php
@@ -7,15 +7,8 @@ use Nette\DI\CompilerExtension;
 
 final class VubEplatbyModuleExtension extends CompilerExtension implements TranslationProviderInterface
 {
-    private $defaults = [];
-
     public function loadConfiguration()
     {
-        $builder = $this->getContainerBuilder();
-
-        // set default values if user didn't define them
-        $this->config = $this->validateConfig($this->defaults);
-
         // load services from config and register them to Nette\DI Container
         $this->compiler->loadDefinitionsFromConfig(
             $this->loadFromFile(__DIR__.'/../config/config.neon')['services']
